fc4a639be805915b6101f7bfbaa3b155d1362f32
SunZekai <sunzekai795@163.com>
2024-09-07 23:53:45 +0800

    第二次根据说明的修改

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -29,10 +29,10 @@ module.exports = [
       needComments: true,
       // 其他的 Vssue 配置
       autoCreateIssue: true,
-      clientId: "adb9fb0ac1159e00ce7f",
-      clientSecret: "27da8dc85f808c2bd1b6e44da5ae69c4ddf17d8d",
-      owner: "eryajf",
-      repo: "vdoing-template",
+      clientId: "Ov23liHK9xSSaGV8vhew",
+      clientSecret: "6c935bd520c89d92f6989682ccdf13fda0a45190",
+      owner: "sunzekai795",
+      repo: "sunzekai795.github.io",
     },
   ],
   [
