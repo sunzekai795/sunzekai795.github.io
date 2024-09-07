22aa87e51b31eda39e6fc50ac2a4629ce6a53d37
SunZekai <sunzekai795@163.com>
2024-09-07 23:38:13 +0800

    第一次根据说明的修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "szk-blogs",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/sunzekai795.github.io/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -14,4 +14,34 @@ module.exports = {
   head,
   plugins,
   themeConfig,
+
+  plugins: [
+    // vssue 评论插件
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "Ov23liHK9xSSaGV8vhew",
+        clientSecret: "6c935bd520c89d92f6989682ccdf13fda0a45190",
+        owner: "sunzekai795",
+        repo: "sunzekai795.github.io",
+      },
+    ],
+    // Markdown 增强
+    [
+      "md-enhance",
+      {
+        // 启用流程图
+        flowchart: true,
+        // Enable mermaid
+        mermaid: true,
+        // 启用 TeX 支持
+        tex: true,
+      },
+    ],
+  ],
 }
--- docs/index.md
+++ docs/index.md
@@ -1,12 +1,13 @@
 ---
 home: true
-heroImage: https://cdn.jsdelivr.net/gh/eryajf/tu/img/image_20220720_132208.png
-tagline: Vdoing博客主题模板
-heroText: vdoing-template
-actionText: 开始浏览 →
-actionLink: /pages/24768e/
-bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+heroImage: C:/Codes/projects/MyBlog/lql-notes/images/0e69cd48d33675a9b04b77a315408ffa52f31cd6.jpg@1256w_680h_!web-article-pic.jpg
+tagline: Szk's Blogs
+heroText: szk-notes
+# actionText: 开始浏览 →
+# actionLink: /pages/24768e/
+bannerBg: auto # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
 
 # 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
 postList: simple
+simplePostListLength: 10 # 简约版文章列表显示的文章数量
 ---
